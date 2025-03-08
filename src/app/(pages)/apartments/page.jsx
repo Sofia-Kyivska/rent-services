3b42d26ec21b428d4e367ebd67ca88373991fd5c
volodymirkavun1 <volodymirkavun1@gmail.com>
2025-03-08 14:32:47 +0200

    add: seo_img+meta

--- src/app/(pages)/apartments/page.jsx
+++ src/app/(pages)/apartments/page.jsx
@@ -30,44 +30,44 @@ export async function generateMetadata({ params }) {
     title,
     description,
     keywords,
-    // alternates: {
-    //   canonical: `${process.env.NEXT_PUBLIC_SEO_URL}apartments`,
-    // },
+    alternates: {
+      canonical: `${process.env.NEXT_PUBLIC_SEO_URL}apartments`,
+    },
   };
 }
 
 
 const Apartments = () => {
-  // const jsonLd = {
-  //   "@context": "http://schema.org",
-  //   "@type": "BreadcrumbList",
-  //   itemListElement: [
-  //     {
-  //       "@type": "ListItem",
-  //       position: 1,
-  //       item: {
-  //         "@id": process.env.NEXT_PUBLIC_SEO_URL,
-  //         name: "Sofia Rent Kyiv - оренда квартири Київ. Квартири подобово.",
-  //       },
-  //     },
-  //     {
-  //       "@type": "ListItem",
-  //       position: 2,
-  //       item: {
-  //         "@id": `${process.env.NEXT_PUBLIC_SEO_URL}apartments`,
-  //         name: "Sofia Rent Kyiv Квартири",
-  //       },
-  //     },
-  //   ],
-  // };
+  const jsonLd = {
+    "@context": "http://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        position: 1,
+        item: {
+          "@id": process.env.NEXT_PUBLIC_SEO_URL,
+          name: "Sofia Rent Kyiv - оренда квартири Київ. Квартири подобово.",
+        },
+      },
+      {
+        "@type": "ListItem",
+        position: 2,
+        item: {
+          "@id": `${process.env.NEXT_PUBLIC_SEO_URL}apartments`,
+          name: "Sofia Rent Kyiv Квартири",
+        },
+      },
+    ],
+  };
 
 
   return (
     <>
-      {/* <script
+      <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-      /> */}
+      />
       <ApartmentsComponent />
     </>
   );
--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ const DynamicAuthProvider = dynamic(() => import('@/components/AuthProvider/Auth
 
 
 export const metadata = {
-  // metadataBase: new URL(process.env.NEXT_PUBLIC_SEO_URL),
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SEO_URL),
   title: 'Sofia Rent Kyiv - оренда квартири Київ. Квартири подобово.',
   description:
     'Київ квартири ⭐ Зняти квартиру Київ ✔️ Оренда квартири Київ 🔑 Квартири подобово 📅 Квартири на день',
@@ -38,57 +38,57 @@ export const metadata = {
     'Киев квартиры',
     'Аренда квартир Киев посуточно',
   ],
-  // alternates: {
-  //   canonical: process.env.NEXT_PUBLIC_SEO_URL,
-  // },
-  // themeColor: '#373737',
-  // openGraph: {
-  //   title: 'Sofia Rent - оренда квартири Київ. Квартири подобово.',
-  //   url: process.env.NEXT_PUBLIC_SEO_URL,
-  //   description:
-  //     'Київ квартири ⭐ Зняти квартиру Київ ✔️ Оренда квартири Київ 🔑 Квартири подобово 📅 Квартири на день',
-  //   type: 'website',
-  //   siteName: 'Sofia Rent',
-  //   images: [
-  //     {
-  //       url: '/seo_images/opengraph-image-400x300.png',
-  //       type: 'image/png',
-  //       width: 400,
-  //       height: 300,
-  //       alt: 'Sofia Rent',
-  //     },
-  //     {
-  //       url: '/seo_images/twitter-image-800x600.png',
-  //       type: 'image/png',
-  //       width: 800,
-  //       height: 600,
-  //       alt: 'Sofia Rent',
-  //     },
-  //     {
-  //       url: '/seo_images/opengraph-image-1200-630.png',
-  //       type: 'image/png',
-  //       width: 1200,
-  //       height: 630,
-  //       alt: 'Sofia Rent',
-  //     },
-  //   ],
-  //   locale: 'uk-UA',
-  // },
-  // appLinks: {
-  //   ios: {
-  //     url: process.env.NEXT_PUBLIC_SEO_URL,
-  //     app_name: 'Sofia Rent',
-  //   },
-  //   android: {
-  //     url: process.env.NEXT_PUBLIC_SEO_URL,
-  //     package: process.env.NEXT_PUBLIC_SEO_URL,
-  //     app_name: 'Sofia Rent',
-  //   },
-  //   web: {
-  //     url: process.env.NEXT_PUBLIC_SEO_URL,
-  //     should_fallback: true,
-  //   },
-  // },
+  alternates: {
+    canonical: process.env.NEXT_PUBLIC_SEO_URL,
+  },
+  themeColor: '#131112',
+  openGraph: {
+    title: 'Sofia Rent - оренда квартири Київ. Квартири подобово.',
+    url: process.env.NEXT_PUBLIC_SEO_URL,
+    description:
+      'Київ квартири ⭐ Зняти квартиру Київ ✔️ Оренда квартири Київ 🔑 Квартири подобово 📅 Квартири на день',
+    type: 'website',
+    siteName: 'Sofia Rent',
+    images: [
+      {
+        url: '/seo_images/opengraph-image-400x300.png',
+        type: 'image/png',
+        width: 400,
+        height: 300,
+        alt: 'Sofia Rent',
+      },
+      {
+        url: '/seo_images/twitter-image-800x600.png',
+        type: 'image/png',
+        width: 800,
+        height: 600,
+        alt: 'Sofia Rent',
+      },
+      {
+        url: '/seo_images/opengraph-image-1200-630.png',
+        type: 'image/png',
+        width: 1200,
+        height: 630,
+        alt: 'Sofia Rent',
+      },
+    ],
+    locale: 'uk-UA',
+  },
+  appLinks: {
+    ios: {
+      url: process.env.NEXT_PUBLIC_SEO_URL,
+      app_name: 'Sofia Rent',
+    },
+    android: {
+      url: process.env.NEXT_PUBLIC_SEO_URL,
+      package: process.env.NEXT_PUBLIC_SEO_URL,
+      app_name: 'Sofia Rent',
+    },
+    web: {
+      url: process.env.NEXT_PUBLIC_SEO_URL,
+      should_fallback: true,
+    },
+  },
   // assets: [process.env.NEXT_PUBLIC_SEO_URL],
   // verification: {
   //   google: process.env.NEXT_PUBLIC_GSC,
@@ -97,38 +97,38 @@ export const metadata = {
 
 
 export default function RootLayout({ children }) {
-  // const jsonLd = {
-  //   '@context': 'https://schema.org',
-  //   '@type': 'Organization',
-  //   name: 'Sofia Rent Kyiv',
-  //   url: process.env.NEXT_PUBLIC_SEO_URL,
-  //   contactPoint: [
-  //     {
-  //       '@type': 'ContactPoint',
-  //       telephone: '+380954515057',
-  //       contactType: 'customer service',
-  //     },
-  //   ],
-  //   logo: [
-  //     {
-  //       '@type': 'ImageObject',
-  //       url: '/seo_images/twitter-image-800x600.png',
-  //       contentUrl: '/seo_images/twitter-image-800x600.png',
-  //       size: '800x600',
-  //       caption: 'Sofia Rent Kyiv',
-  //       inLanguage: 'uk-UA',
-  //     },
-  //   ],
-  //   keywords:
-  //     'Київ квартири. Зняти квартиру Київ. Оренда квартири Київ. Квартири подобово. Квартири на день',
-  // };
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    name: 'Sofia Rent Kyiv',
+    url: process.env.NEXT_PUBLIC_SEO_URL,
+    contactPoint: [
+      {
+        '@type': 'ContactPoint',
+        telephone: '+380954515057',
+        contactType: 'customer service',
+      },
+    ],
+    logo: [
+      {
+        '@type': 'ImageObject',
+        url: '/seo_images/twitter-image-800x600.png',
+        contentUrl: '/seo_images/twitter-image-800x600.png',
+        size: '800x600',
+        caption: 'Sofia Rent Kyiv',
+        inLanguage: 'uk-UA',
+      },
+    ],
+    keywords:
+      'Київ квартири. Зняти квартиру Київ. Оренда квартири Київ. Квартири подобово. Квартири на день',
+  };
   return (
     <html lang="uk-UA">
       <body>
-        {/* <script
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        /> */}
+        />
         <SiteProvider>
           <DynamicToastProvider>
             <DynamicAuthProvider>
