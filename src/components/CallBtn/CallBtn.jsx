e1b2ed5a215013963e4ecbde9224a594af742779
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2025-03-12 23:07:16 +0200

    fix bug in hero and translate btn

--- src/components/CallBtn/CallBtn.jsx
+++ src/components/CallBtn/CallBtn.jsx
@@ -1,28 +1,27 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import { useTranslation } from "react-i18next";
-import { socialLinks } from "@/data";
-import styles from "./CallBtn.module.scss";
+'use client';
+import React, { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { socialLinks } from '@/data';
+import styles from './CallBtn.module.scss';
 
-
-const CallBtn = ({ className }) => {
+const CallBtn = ({ className, id }) => {
   const { t } = useTranslation();
   const [isLoading, setIsLoading] = useState(true);
 
   // Найти объект с телефоном
-  const phoneLink = socialLinks.find(link => link.title === "Telephone")?.href;
+  const phoneLink = socialLinks.find(
+    (link) => link.title === 'Telephone'
+  )?.href;
 
   useEffect(() => {
     setIsLoading(false);
   }, []);
 
-
   return (
-    <a href={phoneLink} className={`${styles.button} ${className}`}>
-      {!isLoading && t("Buttons.CalltBtn")}
+    <a href={phoneLink} className={`${styles.button} ${className}`} id={id}>
+      {!isLoading && t('Buttons.CalltBtn')}
     </a>
   );
 };
-
 
 export default CallBtn;
--- src/components/OrderBtn/OrderBtn.jsx
+++ src/components/OrderBtn/OrderBtn.jsx
@@ -1,33 +1,31 @@
-import { useState, useEffect, useContext } from "react";
-import { useTranslation } from "react-i18next";
-import { SiteContext } from "@/context/SiteContext";
-import styles from "./OrderBtn.module.scss";
+import { useState, useEffect, useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import { SiteContext } from '@/context/SiteContext';
+import styles from './OrderBtn.module.scss';
 
+const OrderBtn = ({ className, id }) => {
+  const { t } = useTranslation();
+  const { openModal } = useContext(SiteContext);
+  const [isLoading, setIsLoading] = useState(true);
 
-const OrderBtn = ({ className }) => {
-    const { t } = useTranslation();
-    const { openModal } = useContext(SiteContext);
-    const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
 
-    useEffect(() => {
-        setIsLoading(false);
-    }, []);
-
-
-    return (
-        <>
-            {!isLoading && (
-                <button
-                    type='button'
-                    className={styles.button + " " + `${className}`}
-                    onClick={openModal}
-                >
-                    {t("Buttons.OrderBtn")}
-                </button>
-            )}
-        </>
-    );
+  return (
+    <>
+      {!isLoading && (
+        <button
+          type="button"
+          className={styles.button + ' ' + `${className}`}
+          onClick={openModal}
+          id={id}
+        >
+          {t('Buttons.OrderBtn')}
+        </button>
+      )}
+    </>
+  );
 };
-
 
 export default OrderBtn;
